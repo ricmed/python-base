c1174bc1ca03a2437d90dfcb27a03857d98d7308
Ricardo Medeiros <ricmed@gmail.com>
2022-05-08 13:59:00 -0300

    Implementamos o hello word multi idiomas

--- hello.py
+++ hello.py
@@ -1,2 +1,35 @@
 #!/usr/bin/env python3
-print('Ricardo'.upper())
+""""Hello World Multi Linguas
+
+Dependendo da líguo configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt-br
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Ricardo Medeiros"
+__license__ = "Unlicense"
+
+#Dunder - indica 2 underlines antes e depois da palavra
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5] # idioma padrão en_US
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mundo!"
+print(msg)
